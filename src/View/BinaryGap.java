182d39840f3d1f93c71ccea3e4c7d63c4a7ceebc
Claud Sumbus <claudiosumburane@gmail.com>
2018-03-17 16:53:26 +0200

    clean liness

--- src/View/BinaryGap.java
+++ src/View/BinaryGap.java
@@ -1,7 +1,6 @@
 package View;
 
 import Controller.BinaryGapController;
-import static jdk.nashorn.internal.runtime.GlobalFunctions.parseInt;
 
 /**
  *
@@ -12,12 +11,10 @@ public class BinaryGap {
     public static void main(String[] args) {
 
         BinaryGapController controller = new BinaryGapController();
-
         
         int array[] = {1,0,1,0,1,0,0};   
         
         System.out.println(controller.recebeArray(array));
         
-        System.out.println(parseInt(1000, 10));
     }
 }
